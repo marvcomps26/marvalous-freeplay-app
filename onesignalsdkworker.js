2ad38fa74a354987a1b7e8240da576e838ad4eb8
marvcomps26 <marvcomps26@gmail.com>
2026-05-29 09:11:44 +0100

    Update onesignalsdkworker.js

--- onesignalsdkworker.js
+++ onesignalsdkworker.js
@@ -1,13 +1,1 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-self.addEventListener("install", event => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
-});
